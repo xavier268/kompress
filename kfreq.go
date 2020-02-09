605e537b66014770be6ca5758858152155bc4584
xavier268 <xavier@Fedora31OnDellXPS>
2020-02-09 07:50:21 +0100

    dynamic escape selection

---
+++ kfreq.go
@@ -0,0 +1,31 @@
+package kompress
+
+type kfreq struct {
+	// occurences of each byte so far
+	freq [256]int
+	// sum of all occurrence
+	sum int
+	// currently selected escape byte
+	// you may read it directly
+	// but only use update to update it.
+	esc byte
+}
+
+// update the frequency table with the byte provided
+// Will also uupdate the esc value, as needed.
+func (f *kfreq) update(b byte) {
+	f.sum++
+	f.freq[b]++
+	if b == f.esc {
+		// We need to revalidate the esc value
+		for i := range f.freq {
+			if f.freq[f.esc] > f.freq[i] {
+				// If we found a lower occurence value, we retain it.
+				// There acn not be better values, only same values,
+				// so we can safely return immediately.
+				f.esc = byte(i)
+				return
+			}
+		}
+	}
+}
