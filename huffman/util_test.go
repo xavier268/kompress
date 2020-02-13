ee20915fef8a7ca89140af0552d7c2f7d18ad093
xavier268 <xavier@Fedora31OnDellXPS>
2020-02-13 17:40:33 +0100

    test

--- huffman/util_test.go
+++ huffman/util_test.go
@@ -1,7 +1,7 @@
 package huffman
 
 import (
-	"fmt"
+	"io"
 	"testing"
 )
 
@@ -20,6 +20,29 @@ func TestBitBuffer(t *testing.T) {
 	if err = bb.WriteBit(1); err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println("Size : ", bb.Size())
+	if bb.Size() != 3 {
+		t.Fatal("wrong size : ", bb.Size())
+	}
+
+	b, err := bb.ReadBit()
+	if b != 1 || err != nil {
+		t.Fatal("invalid : ", b, err)
+	}
+	b, err = bb.ReadBit()
+	if b != 0 || err != nil {
+		t.Fatal("invalid : ", b, err)
+	}
+	b, err = bb.ReadBit()
+	if b != 1 || err != nil {
+		t.Fatal("invalid : ", b, err)
+	}
+	if bb.Size() != 0 {
+		t.Fatal("wrong size : ", bb.Size())
+	}
+
+	_, err = bb.ReadBit()
+	if err != io.EOF {
+		t.Fatal("exected io.EOF, but got ", err)
+	}
 
 }
