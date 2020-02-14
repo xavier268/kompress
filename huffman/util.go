9c61d46e924ad07b6c30d4db2349b552f5beb313
xavier268 <xavier@Fedora31OnDellXPS>
2020-02-14 17:59:56 +0100

    bit2byte tests ok

--- huffman/util.go
+++ huffman/util.go
@@ -45,7 +45,8 @@ func (l *LogWriter) WriteSymbol(s Symbol) error {
 
 // ========================  BitBuffer ============================
 
-// A BitBuffer can be read or written bits.
+// A BitBuffer can be read or written bits, or even bytes.
+// It size adapts dynamically.
 type BitBuffer struct {
 	bits []Bit
 }
@@ -129,4 +130,114 @@ func (bb *BitBuffer) WriteByte(b byte) error {
 		mask = mask / 2
 	}
 	return err
+}
+
+// =============== BitFromByteReader ======================
+
+// A BitFromByteReader object encapsulates an io.Reader and provides
+// bit read . Internal buffer is kept to a minimum
+// (less than 8 bits).
+type BitFromByteReader struct {
+	reader io.Reader
+	// mask pointing to next available bit
+	mask byte
+	// buffer
+	buf [1]byte
+}
+
+// compiler cheks
+var _ BitReader = new(BitFromByteReader)
+
+// NewBitFromByteReader constrstructed from io.Reader.
+// Performance buffering is up to the io.Reader.
+func NewBitFromByteReader(ior io.Reader) *BitFromByteReader {
+	r := new(BitFromByteReader)
+	r.reader = ior
+	return r
+}
+
+// ReadBit from the underlying io.Reader.
+func (r *BitFromByteReader) ReadBit() (Bit, error) {
+
+	if r.mask <= 0 {
+		r.mask = 0b_1000_0000
+		_, err := r.reader.Read(r.buf[:])
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	bit := r.buf[0] & r.mask
+	r.mask = r.mask >> 1
+	if bit == 0 {
+		return 0, nil
+	}
+	return 1, nil
+
+}
+
+// =============== BitToByteWriter ======================
+
+// A BitToByteWriter object encapsulates an io.Writer  and provides
+// bit write capabilities. Internal buffer is kept to a minimum
+// (less than 8 bits). Closing writes is essential to trigger padding  and flush.
+type BitToByteWriter struct {
+	writer io.Writer
+	// mask pointing to next available bit
+	mask byte
+	// buffer
+	buf [1]byte
+}
+
+// compiler cheks
+var _ BitWriteCloser = new(BitToByteWriter)
+
+// NewBitToByteWriter from an io.Writer.
+func NewBitToByteWriter(ior io.Writer) *BitToByteWriter {
+	w := new(BitToByteWriter)
+	w.mask = 0b_1000_0000
+	w.writer = ior
+	return w
+}
+
+// WriteBit a single bit, and flush if full byte is available.
+func (w *BitToByteWriter) WriteBit(b Bit) (err error) {
+
+	if b != 0 {
+		w.buf[0] |= w.mask
+	}
+
+	w.mask = w.mask >> 1
+	if w.mask == 0 {
+		w.mask = 128
+		_, err = w.writer.Write(w.buf[:])
+		w.buf[0] = 0
+	}
+
+	return err
+
+}
+
+// WriteBits zero or more bits,  flushing every full byte available.
+func (w *BitToByteWriter) WriteBits(b ...Bit) (err error) {
+	for i := range b {
+		err = w.WriteBit(b[i])
+	}
+	return err
+}
+
+// Close and flush, padding with 0.
+// It tries to close the underlying io.Writer if it is also a Closer.
+func (w *BitToByteWriter) Close() (err error) {
+
+	// Write 0 until pending byte is flushed
+	for w.mask != 128 {
+		err = w.WriteBit(0)
+	}
+
+	if c, ok := w.writer.(io.Closer); ok {
+		return c.Close()
+	}
+	return err
+
 }
--- huffman/util_test.go
+++ huffman/util_test.go
@@ -1,6 +1,7 @@
 package huffman
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"testing"
@@ -72,4 +73,88 @@ func TestBitBufferBytes2Padding(t *testing.T) {
 		fmt.Printf("From %08b   ==>   %08b\n", s, ss)
 		t.Fatal(err, bb)
 	}
+}
+
+func TestBitFromByteReader(t *testing.T) {
+
+	buf := []byte{0b1100_0010}
+	ior := bytes.NewBuffer(buf)
+	r := NewBitFromByteReader(ior)
+
+	b, err := r.ReadBit()
+	if b != 1 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+	b, err = r.ReadBit()
+	if b != 1 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+	b, err = r.ReadBit()
+	if b != 0 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+	b, err = r.ReadBit()
+	if b != 0 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+
+	b, err = r.ReadBit()
+	if b != 0 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+	b, err = r.ReadBit()
+	if b != 0 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+	b, err = r.ReadBit()
+	if b != 1 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+	b, err = r.ReadBit()
+	if b != 0 || err != nil {
+		t.Fatal("wrong bit or error ", b, err)
+	}
+
+	// expect  error ...
+	b, err = r.ReadBit()
+	if b != 0 || err != io.EOF {
+		t.Fatal("wrong bit", b)
+	}
+}
+
+func TestBitToByteWriter(t *testing.T) {
+
+	var source []Bit
+	var is, should []byte
+
+	source = []Bit{1, 0, 0, 1,
+		1, 1, 0, 1,
+		0, 1, 1, 0,
+		1, 1}
+	should = []byte{0b_1001_1101, 0b_0110_1100}
+
+	buf := bytes.NewBuffer(is)
+
+	w := NewBitToByteWriter(buf)
+	err := w.WriteBits(source...)
+
+	// Before close ...
+	if bytes.Compare(should[:1], buf.Bytes()) != 0 || err != nil {
+		fmt.Println("\nsource:", source)
+		fmt.Println("got before close:", buf.Bytes())
+		fmt.Println("want:", should)
+		fmt.Println("error:", err)
+		t.Fatal(err)
+	}
+
+	// close
+	w.Close()
+	if bytes.Compare(should, buf.Bytes()) != 0 || err != nil {
+		fmt.Println("\nsource:", source)
+		fmt.Println("got:", buf.Bytes())
+		fmt.Println("want:", should)
+		fmt.Println("error:", err)
+		t.Fatal(err)
+	}
+
 }
