f3b6f2f4c255bbc45d2ee6f1c6993d44756aa4a8
xavier268 <xavier@Fedora31OnDellXPS>
2020-02-14 21:36:04 +0100

    renaming files

---
+++ huffman/engine_test.go
@@ -0,0 +1,26 @@
+package huffman
+
+import "testing"
+
+func TestBasicEngine(t *testing.T) {
+
+	freq := []int{1, 5, 1, 2, 2, 12, 3}
+
+	e := newEngine(freq, 0)
+	// check reasonable balance in tree
+
+	wr := e.root.weight
+	w0 := e.root.child0.weight
+	w1 := e.root.child1.weight
+
+	if wr != w0+w1 {
+		t.Fatal("root weights do not match : ", w0, w1, wr)
+	}
+
+	if (w0*w1 != 12*14) || (w0+w1 != 12+14) {
+		t.Fatal("weights do not match : ", w0, w1)
+	}
+
+	// t.Skip()
+	e.Dump()
+}
--- huffman/tree_test.go
+++
@@ -1,9 +0,0 @@
-package huffman
-
-import "testing"
-
-func TestBasicTreeDump(t *testing.T) {
-	freq := []int{1, 5, 0, 2, 2, 12, 3}
-	e := newEngine(freq, 0)
-	e.Dump()
-}
